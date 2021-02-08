756aa15e7c802e3f0dc99b0c1aec9a2576c6377f
Uwe van Heesch <uwe@vanheesch.net>
2021-02-08 17:55:57 +0100

    Make things uglier

--- src/main/java/nl/han/dea/PriceCalculator.java
+++ src/main/java/nl/han/dea/PriceCalculator.java
@@ -4,19 +4,23 @@ public class PriceCalculator {
 
     public double calculatePriceForTomatoes(int type, int quantity) {
         if (type == 1) { // Big Beef Tomato 0,23 Euro/piece
-            return quantity * 0.23;
+            var price = quantity * 0.23;
+            return price;
         }
         if (type == 2) { // Cherry tomatoes 0,12 Euro/piece
-            return quantity * 0.14;
+            var price = quantity * 0.14;
+            return price;
         }
         if (type == 4) {
-            return quantity * 0.23;
+            var price = quantity * 0.23;
+            return price;
         }
         return -1;
     }
 
     public double calculatePriceForApples(int type, int quantity) {
         if (type == 1) { // Sweet Tango
+
             return quantity * 0.35;
         }
         if (type == 2) { // McIntosh
